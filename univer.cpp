4a9177f84c66f488a05ff172f22fda7d3d286056
Tanya <tani.netu@yandex.ru>
2014-06-18 18:02:23 +0700

    Update univer.cpp

--- univer.cpp
+++ univer.cpp
@@ -23,26 +23,25 @@ void readFile(const char *name)
 {
 	string line, replacement;
 	std::tr1::regex rx;
-	string rep[10]={"Academy", "Institute", "Sciences", "USSR", "Semiconductor", "Physics", "Russian Academy Medical Scienses", "Medical", "Novosibirsk ", "Russia "};
-	string reg[10]={"Acad.", "Inst.", "Sci.", "U.S.S.R.", "Semicond.", "Phys.", "RAMS", "Med.","Novosibirsk.", "Russia."};
+	string rep[]={"Academy", "Institute", "Sciences", "USSR", "Semiconductor", "Physics", "Russian Academy Medical Scienses", "Medical", "Novosibirsk ", "Russia ", "", "", "", "", ""};//
+	string reg[]={"Acad.", "Inst.", "Sci.", "U.S.S.R.", "Semicond.", "Phys.", "RAMS", "Med.","Novosibirsk.", "Russia.", "and", "of", "the", "all", "for"};
 	string::iterator it;
 	int i;
-
 	ifstream in(name); 
 	if(!in) 
-        {
+    {
 		cout << "The file \"" << name << "\" cannot be opened.\n";
 	}
     while(in)
 	{
 		while(getline(in,line))
 		{
-			for(i=0; i<10; i++)
+			for(i=0; i<15; i++)
 			{
 				rx = reg[i];
 				replacement = rep[i];
-                                line = regex_replace(line, rx, replacement);
-			}
+                line = regex_replace(line, rx, replacement);
+			}			
 			for (it = line.begin(); it != line.end(); it++)
 			{
 			  if ((*it) == '-') it = line.erase (it);
